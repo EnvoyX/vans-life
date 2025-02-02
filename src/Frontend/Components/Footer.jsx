961a841a8b46d042bfc3f96aa4b1282d5f562978
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-02-02 12:43:10 +0700

    Delete src/Frontend/Components directory

--- src/Frontend/Components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full h-[74px] bg-[#161616] flex justify-center items-center text-[#AAAAAA] mt-auto">
-      &#169; 2025 #VANLIFE
-    </footer>
-  );
-}
